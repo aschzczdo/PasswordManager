c0d275bf9b3e0690f478e140be6a3312ce5d7bea
Angel <77752887+aschezczdo@users.noreply.github.com>
2023-05-02 20:43:33 +0200

    Release 0.0.1

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,23 @@ import AES.Encrypt;
 import AES.SecretKey;
 
 import javax.crypto.spec.SecretKeySpec;
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 public class Main {
 
-    public static void main(String[] args) {
-        ui.LoginUI.main(args);
+    public static void main(String[] args) throws URISyntaxException, IOException {
+        try {
+            ui.LoginUI.main(args);
+        } catch (UnsupportedClassVersionError e) {
+            System.out.println("Error, Java class not supported. Download Java 19 please.");
+            String  linkDescarga = "https://www.java.com/en/download/";
+            Desktop.getDesktop().browse(new URI(linkDescarga));
+        }
+
 
     }
 }
 
-        
